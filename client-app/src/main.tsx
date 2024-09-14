23d5e75b62562afc6ad988279baab49025c12949
demian824 <demian824@gmail.com>
2024-09-14 16:50:09 -0400

    End of Session 7

--- client-app/src/main.tsx
+++ client-app/src/main.tsx
@@ -1,11 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './app/layout/App.tsx'
+
 import 'semantic-ui-css/semantic.min.css'
 import './app/layout/styles.css'
+import { store, StoreContext } from './app/stores/store.ts'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <StoreContext.Provider value={store}>
+      <App />
+    </StoreContext.Provider>
+    
+  </React.StrictMode>,
 )
