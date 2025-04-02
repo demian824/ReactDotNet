0e19e6a27adf2dde93a18608c451d5852dfa43b9
demian824 <demian824@gmail.com>
2025-04-02 15:30:26 -0400

    End of section 15

--- client-app/src/feature/activities/form/ActivityForm.tsx
+++ client-app/src/feature/activities/form/ActivityForm.tsx
@@ -1,12 +1,12 @@
-import { ChangeEvent, useEffect, useState } from 'react';
-import { Button, Header, Label, Segment } from 'semantic-ui-react';
+import { useEffect, useState } from 'react';
+import { Button, Header, Segment } from 'semantic-ui-react';
 import { useStore } from '../../../app/stores/store';
 import { observer } from 'mobx-react-lite';
 import { Link, useNavigate, useParams } from 'react-router-dom';
-import { Activity } from '../../../app/models/activity';
+import { ActivityFormValues } from '../../../app/models/activity';
 import LoadingComponent from '../../../app/layout/LoadingComponent';
 import {v4 as uuid} from 'uuid';
-import { Formik, Form, ErrorMessage } from 'formik';
+import { Formik, Form } from 'formik';
 import * as Yup from 'yup';
 import MyTextInput from '../../../app/common/form/MyTextInput';
 import MyTextArea from '../../../app/common/form/MyTextArea';
@@ -16,18 +16,10 @@ import MyDateInput from '../../../app/common/form/MyDateInput';
 
 export default observer( function ActivityForm(){
     const {activityStore} = useStore();
-    const {loading, loadingInitial, createActivity, updateActivity, loadActivity} = activityStore
+    const {loadingInitial, createActivity, updateActivity, loadActivity} = activityStore
     const {id} = useParams();
     const navigate = useNavigate();
-    const[activity, setActivity] = useState<Activity>({
-        id:'',
-        title:'',
-        category:'',
-        description:'',
-        date: null,
-        city:'',
-        venue:''   
-    });
+    const[activity, setActivity] = useState<ActivityFormValues>(new ActivityFormValues());
 
     const validationSchema = Yup.object({
         title: Yup.string().required('The activity title is required'),
@@ -39,11 +31,11 @@ export default observer( function ActivityForm(){
     })
 
     useEffect(() =>{
-        if (id) loadActivity(id).then(activity=> setActivity(activity!)); // ! : Definite Assignment Assertions
+        if (id) loadActivity(id).then(activity=> setActivity(new ActivityFormValues(activity))); // ! : Definite Assignment Assertions
     },[id,loadActivity]);
 
-    function handleFormSubmit(activity: Activity){
-        if(activity.id.length === 0) {
+    function handleFormSubmit(activity: ActivityFormValues){
+        if(!activity.id) {
             let newActivity = {
                 ...activity, 
                 id: uuid()
@@ -82,7 +74,7 @@ export default observer( function ActivityForm(){
                         <MyTextInput placeholder='Venue' name='venue' />
                         <Button 
                             disabled={isSubmitting || !dirty || !isValid}
-                            loading={loading} 
+                            loading={isSubmitting} 
                             floated='right' 
                             positive type='submit' 
                             content='Submit' />
