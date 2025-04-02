c01b194348a2395f1820739455cd5eef2e980301
demian824 <demian824@gmail.com>
2025-04-02 12:30:09 -0400

    End of section 14

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -12,7 +12,7 @@ namespace API.Controllers
         public async Task<IActionResult>GetActivities(){
             return HandleResult(await Mediator.Send(new List.Query()));
         }
-        [Authorize]
+        [Authorize(Policy = "IsActivityHost")]
         [HttpGet("{id}")]
         public async Task<IActionResult>GetActivity(Guid id){
             return HandleResult(await Mediator.Send(new Details.Query{Id = id}));
@@ -23,16 +23,22 @@ namespace API.Controllers
 
             return Ok();
         }
+        [Authorize(Policy = "IsActivityHost")]
         [HttpPut("{id}")]
         public async Task<IActionResult>EditActivity(Guid id, Activity activity){
             activity.Id = id;
             return HandleResult(await Mediator.Send(new Edit.Command{ Activity = activity }));
         }
-
+        [Authorize(Policy = "IsActivityHost")]
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteActivity(Guid id){
             
             return HandleResult(await Mediator.Send(new Delete.Command { Id = id}));
+        }
+        [HttpPost("{id}/attend")]
+        public async Task<IActionResult> Attend(Guid id)
+        {
+            return HandleResult(await Mediator.Send(new UpdateAttendance.Command{Id = id}));
         }
     }
 }
