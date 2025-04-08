86b15b0fe23be0f99092d703bd251c57fd89420c
demian824 <demian824@gmail.com>
2025-04-08 16:03:23 -0400

    End of Session 18

--- client-app/src/app/stores/userStore.ts
+++ client-app/src/app/stores/userStore.ts
@@ -43,6 +43,9 @@ export default class UserStore{
             this.user.image = image;
         }
     }
+    setDisplayName = (name: string) => {
+        if(this.user) this.user.displayName = name;
+    }
     logout = () =>{
         store.commonStore.setToken(null);
         
